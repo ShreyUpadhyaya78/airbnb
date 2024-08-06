057b7bd12449d1ac2c083e39589aae390ed4c733
ShreyUpadhyaya78 <076bct078.shrey@pcampus.edu.np>
2024-08-06 15:54:00 +0545

    Initial Commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
+import UserNav from "./UserNav";
+export default function Navbar() {
+  return (
+    <nav className="w-full border-b">
+
+<div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+<Link href='/'>
+<Image src={DesktopLogo} alt="Desktop Logo" className="w-32 hidden lg:block">
+</Image>
+<Image src={MobileLogo} alt="Mobile Logo" className="block w-12 lg:hidden"></Image>
+</Link>
+<div className="rounded-full border px-5 py-2">
+  Hello from search
+</div>
+<UserNav></UserNav>
+</div>
+    </nav>
+  )
+}
